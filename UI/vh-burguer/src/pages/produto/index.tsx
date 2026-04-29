f26704f6ed463f9205118b53f94c6fe8be37822a
guirrs <guirsan6os189@gmail.com>
2026-04-29 10:29:23 -0300

    multipla escolha categoria UI

--- UI/vh-burguer/src/pages/produto/index.tsx
+++ UI/vh-burguer/src/pages/produto/index.tsx
@@ -3,19 +3,24 @@ import SubHeader from "@/components/sub-header/sub-header";
 import styles from "./produto.module.css"
 import { useEffect, useState } from "react";
 import { listarCategoria } from "../api/categoriaService";
-import { Console } from "console";
 
-interface categoria {
+interface Categoria {
     categoriaID: number,
-    categoriaNome: string
+    nome: string
 }
 
 const Produto = () => {
 
-    const[categorias, setCategorias] = useState<categoria[]>([]);
+    const [categorias, setCategorias] = useState<Categoria[]>([]);
 
-     function listarCategoriaEmProduto() {
-        const lista = listarCategoria();
+    const [nome, setNome] = useState<string>("");
+    const [descricao, setDescricao] = useState<string>("");
+    const [preco, setPreco] = useState<string>("");
+    const [imagem, setImagem] = useState<File | null>();
+    const [categoriasSelecionadas, setCategoriasSelecionadas] = useState<number[]>([]);
+
+    async function listarCategoriaEmProduto() {
+        const lista = await listarCategoria();
 
         //caso voce só chama a lista, vai chamar toda a requisicao, por isso que voce tem que especificar só quer a lista do dados
         setCategorias(lista.data);
@@ -24,7 +29,7 @@ const Produto = () => {
     }
 
     //quando produto for renderizado, a funcao listarCategoriaEmProduto acontece
-    useEffect(() =>{
+    useEffect(() => {
         listarCategoriaEmProduto();
     }, [])
 
@@ -32,36 +37,43 @@ const Produto = () => {
         <>
             <SubHeader />
             <section id={styles.container}>
-                    <h3>CRIAR PRODUTO</h3>
-                    <div className={styles.info}>
-                        <label>Nome do produto</label>
-                        <input type="text" placeholder="BBQ Especial"/>
-                    </div>
-                    <div className={`${styles.info} ${styles.descricao}`}>
-                        <label>Descrição</label>
-                        <input type="text" placeholder="Hamburguer com molho barbecue defumado com cebola caramelizada."/>
-                    </div>
-                    <div className={`${styles.info} ${styles.preco}`}>
-                        <label>Preço(R$)</label>
-                        <input type="text" placeholder="40,00"/>
-                    </div>
-                    <div className={styles.info}>
-                        <label>Categoria</label>
-                        <select>
-                            {categorias.map((item) => (
-                                <option value={item.categoriaID} key={item.categoriaID}>{item.categoriaNome}</option>
-                            )
-                            )}
-                        </select>
-                    </div>
-                    <div className={`${styles.info} ${styles.url}`}>
-                        <label>Url Imagem</label>
-                        <input type="text" placeholder="https://unsplash.com/pt-br/fotografias/cheseburger-de-"/>
-                    </div>
+                <h3>CRIAR PRODUTO</h3>
+                <div className={styles.info}>
+                    <label>Nome do produto</label>
+                    <input type="text" placeholder="BBQ Especial" value={nome} onChange={(e) => setNome(e.target.value)} />
+                </div>
+                <div className={`${styles.info} ${styles.descricao}`}>
+                    <label>Descrição</label>
+                    <input type="text" placeholder="Hamburguer com molho barbecue defumado com cebola caramelizada." value={descricao} onChange={(e) => setDescricao(e.target.value)}/>
+                </div>
+                <div className={`${styles.info} ${styles.preco}`}>
+                    <label>Preço(R$)</label>
+                    <input type="text" placeholder="40,00" value={preco} onChange={(e) => setPreco(e.target.value)}/>
+                </div>
+                <div className={styles.info}>
+                    <label>Categoria</label>
+                    <select multiple onChange={(e) => setCategoriasSelecionadas(
+                        
+                    )}>
+                        {categorias.map((item) => (
+                            <option value={item.categoriaID} key={item.categoriaID}>{item.nome}</option>
+                        )
+                        )}
+                    </select>
+                </div>
+                <div className={`${styles.info} ${styles.url}`}>
+                    <label>Url Imagem</label>
+                    <input type="file" onChange={(e) => {
+                        if(e.target.files && e.target.files[0]){
+                            setImagem(e.target.files[0]);
+                        }
 
-                    <div id={styles.botoes}>
-                        <button>Salvar</button>
-                    </div>
+                    }} />
+                </div>
+
+                <div id={styles.botoes}>
+                    <button>Salvar</button>
+                </div>
             </section>
             <Footer />
         </>
